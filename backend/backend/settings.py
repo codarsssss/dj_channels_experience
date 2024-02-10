c8aebbd217286d5bdeeb748db225e60895b15251
codarsssss <codarsssss@gmail.com>
2024-02-11 02:41:29 +0300

    done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,6 +116,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND' 'channels.layers.InMemoryChannelLayer'
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     }
 }
